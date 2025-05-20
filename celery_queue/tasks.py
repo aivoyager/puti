04e134683ccb21432821810da1c3bb6d7a0e1960
obstacles <obstaclesws@qq.com>
2025-05-20 18:36:18 +0800

    beat task debug

--- celery_queue/tasks.py
+++ celery_queue/tasks.py
@@ -20,7 +20,6 @@ from celery import shared_task
 from llm.roles.cz import CZ
 from tenacity import retry, stop_after_attempt, wait_fixed, RetryCallState
 
-
 lgr = logger_factory.default
 cz = CZ()
 
@@ -42,29 +41,22 @@ def add(x, y):
 # @celery_app.task(task_always_eager=False)
 @shared_task()
 def periodic_post_tweet():
-
-    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1), reraise=True)
-    def post_tweet(retry_state: RetryCallState = None):
-        attempt_number = retry_state.attempt_number if retry_state else 1
-
-        lgr.debug(f'============== [定时任务] periodic_post_tweet 开始执行第{attempt_number}次 ==============')
-        try:
-            loop = asyncio.get_event_loop()
-            tweet = loop.run_until_complete(cz.run('give me a tweet'))
-            tweet = json.loads(tweet)['final_answer']
-
-            lgr.debug(f'[定时任务] 准备发送推文内容: {tweet}')
-            result = loop.run_until_complete(api.post_tweet(tweet))
-            lgr.debug('[定时任务] 耗时: {:.2f}s'.format(
-                (datetime.now() - start_time).total_seconds()
-            ))
-            lgr.debug(f"[定时任务] 定时任务第{attempt_number}次执行成功: {result}")
-        except Exception as e:
-            lgr.debug(f'[定时任务] 任务第{attempt_number}次执行失败: {e.__class__.__name__} {str(e)}. {traceback.format_exc()}')
-        finally:
-            lgr.debug(f'============== [定时任务] periodic_post_tweet 第{attempt_number}次执行结束 ==============')
-
     api = TwitterAPI()
     start_time = datetime.now()
-    post_tweet()
+    try:
+        loop = asyncio.get_event_loop()
+        tweet = loop.run_until_complete(cz.run('give me a tweet'))
+        tweet = json.loads(tweet)['final_answer']
+        lgr.debug(f'[定时任务] 准备发送推文内容: {tweet}')
+        
+        @retry(stop=stop_after_attempt(3), wait=wait_fixed(1), reraise=True)
+        def safe_post_tweet():
+            return loop.run_until_complete(api.post_tweet(tweet))
+        result = safe_post_tweet()
+        lgr.debug('[定时任务] 耗时: {:.2f}s'.format((datetime.now() - start_time).total_seconds()))
+        lgr.debug(f"[定时任务] 定时任务执行成功: {result}")
+    except Exception as e:
+        lgr.debug(f'[定时任务] 任务执行失败: {e.__class__.__name__} {str(e)}. {traceback.format_exc()}')
+    finally:
+        lgr.debug(f'============== [定时任务] periodic_post_tweet 执行结束 ==============')
     return 'ok'
